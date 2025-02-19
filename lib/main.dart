77ec5504f9e3c14c0b9414e215896fb0d641b0b4
JOEL_ALBERTO_PEREA_CASTORENA <a.22308051281272@cbtis128.edu.mx>
2025-02-19 20:33:27 +0000

    enviar practica 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Perea Flutter',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
@@ -17,25 +18,40 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Perea AppBar'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {}, //lo que esta en llaves son acciones
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+            onPressed: () {},
+          )
+        ],
+        backgroundColor: Colors.green,
         // The title text which will be shown on the action bar
         title: Text(title),
+        leading: const Icon(Icons.bike_scooter),
+        centerTitle: true,
       ),
-      body: Center(
+      body: const Center(
         child: Text(
-          'Hello, World!',
+          'Hola Joel Alberto Perea Castorena con AppBar!',
         ),
       ),
     );
